fddf4bbd6848144768d5d0f80c23890745957033
root <root@ns3024499.ip-149-202-72.eu>
2024-06-06 20:37:07 +0000

    rb

--- app.py
+++ app.py
@@ -4,12 +4,26 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField ,SubmitField
 from wtforms.validators import InputRequired, Length
 from datetime import datetime, timedelta
+import json
+import simplejson
+
+data = {'apple': 'cat', 'banana':'dog', 'pear':'fish'}
+data_json = "{'apple': 'cat', 'banana':'dog', 'pear':'fish'}"
+
+data_json=simplejson.dumps(data)
+print(str(simplejson.dumps(data))) # outputs data
+print(data_json)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+cookie_name=config['DEFAULT']['cookie_name']
+cookie_domain=config['DEFAULT']['cookie_domain']
+cookie_lifespan=int(config['DEFAULT']['cookie_lifespan'])
+cookie_expires=datetime.now() + timedelta(days=cookie_lifespan)
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', render_kw={"placeholder": "Your Name"}, validators=[InputRequired('Username required!'),
@@ -19,16 +33,44 @@ class RegisterForm(FlaskForm):
     receive_alerts = BooleanField('New Announcements', validators=[])
     submit = SubmitField('Submit')
 
-def create_cookie():
-    response=make_response()
-    expires=datetime.now() + timedelta(days=7)
-    response.set_cookie('downcharmed', 'test', expires=expires, domain='.pknw1.co.uk')
+def create_cookie(response,data=None):
+    response=make_response(response)
+    try:
+        cookie_value=request.cookies.get(cookie_name)
+        decoded_data=json.loads(cookie_value)
+    except:
+        decoded_data={'username':'','email':'','registered':'','default':'', 'updates': '', 'notifications': ''}
+    if data == None:
+        data=json.dumps(decoded_data)
+    else:
+        decoded_data.update(data)
+        data=json.dumps(decoded_data)
+    response.set_cookie(cookie_name, data, expires=cookie_expires, samesite='lax', domain=cookie_domain)
     return response
+
+@app.route('/create_cookie')
+def session():
+    response=make_response()
+    data = {'nana': 'nana'}
+    response=create_cookie(response,data)
+    return response
+
+@app.route('/read_cookie/<key>')
+@app.route('/read_cookie')
+def read_cookie(key=None):
+    try:
+        cookie=request.cookies.get(cookie_name)
+        value=json.loads(cookie)
+    except:
+        value={}
+    if key == None:
+        return value
+    else:
+        return value[key]
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form=RegisterForm()
-
     if request.method == 'GET':
         response=render_template('before.html')
         register_form=render_template('register.html', form=form)
@@ -42,24 +84,19 @@ def register():
             updates=request.form['receive_updates']
             updates='Newsletter'
         except:
-            updates='No Newsletter'
+            updates='None'
 
         try:
             notifications=request.form['receive_alerts']
             notifications='Notifications'
         except:
-            notifications='No Notifications'
+            notifications='None'
 
         registration_details=str(request.remote_addr)+' - '+username+' - '+contact+' - '+updates+' - '+notifications+'  registered'
         print(registration_details)
-        expires=datetime.now() + timedelta(days=7)
-        response=make_response()
         response=thanks(username)
-        response=make_response(response)
-        response.set_cookie('downcharmed', 'test', samesite=lax, expires=expires, domain='.pknw1.co.uk')
-
+        response=create_cookie(response,{'username': username, 'email': contact, 'registered': str(datetime.now()), 'updates': updates, 'notifications': notifications})
         return response
-        #return redirect("/thanks/"+username, code=302)
 
 @app.route('/thanks/<username>')
 @app.route('/registered/<username>')
